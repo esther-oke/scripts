24b33b8973308c701fe8730f7074efbba1195c26
Esther Oke <estheroke123@gmail.com>
2020-12-09 16:31:50 +0100

    print out even numbers

--- EvenNumber.py
+++ EvenNumber.py
@@ -3,6 +3,12 @@ This script prints all even numbers between 1 and 100
 '''
 
 # define your function here
-# loop from 1 to 100
-# if the number is even
-# print it
+def even_number():
+    # loop from 1 to 100
+    for i in range(1, 101):
+        
+        # if the number is even
+        if i % 2==0:
+            # print it
+            print(i)
+even_number()    
