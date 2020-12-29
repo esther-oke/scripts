b68ef7724b7651b294d42b81e3b1f02d1f0b991d
Esther Oke <estheroke123@gmail.com>
2020-12-29 18:56:28 +0100

    Calculate the area of a circle

--- CircleArea.py
+++ CircleArea.py
@@ -3,21 +3,25 @@ Write a Python program which accepts the radius of a circle from the user and co
 r = 1.1
 Area = 3.8013271108436504
 '''
+import math
 
 '''
 Declare function that calculates a circle's area given the radius
 '''
 # Declare function header
-
-# calculate area
-
-# return area
-
+def area_of_circle(r):
+    # calculate area
+    ans = math.pi*r**2
+    
+    # return area
+    return ans
 
 # Display instruction to user
-
+print ('this program calculates the area of a circle')
 # Get radius from user
-
+radius = input('radius:')
+radius = float(radius)
 # call function to calculate area
-
+area = area_of_circle(radius)
 # display area
+print('the area of the circle is ',round(area, 2) )
