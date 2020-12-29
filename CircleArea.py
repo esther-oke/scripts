e206c92f63ba781530049ca2df9af8a02785de29
Egahi S. O <okworisamuel6@gmail.com>
2020-12-29 17:49:45 +0100

    Add CircleArea.py

---
+++ CircleArea.py
@@ -0,0 +1,23 @@
+'''
+Write a Python program which accepts the radius of a circle from the user and compute the area.
+r = 1.1
+Area = 3.8013271108436504
+'''
+
+'''
+Declare function that calculates a circle's area given the radius
+'''
+# Declare function header
+
+# calculate area
+
+# return area
+
+
+# Display instruction to user
+
+# Get radius from user
+
+# call function to calculate area
+
+# display area
